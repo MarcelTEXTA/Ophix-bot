0d37cf3e7625b240485c30c9d75cccf91738fb96
MarcelTEXTA <ethanmacaluso265@gmail.com>
2026-04-04 13:54:50 +0200

    Cutomize the logs

--- cogs/logs.py
+++ cogs/logs.py
@@ -20,9 +20,11 @@ class Logs(commands.Cog):
             color=0x57F287
         )
 
+        embed.set_thumbnail(url=guild.icon.url if guild.icon else None)
         embed.add_field(name="Nom", value=guild.name, inline=False)
         embed.add_field(name="ID", value=guild.id, inline=False)
         embed.add_field(name="Membres", value=guild.member_count, inline=False)
+        embed.add_field(name="Nous sommes dans :", value=f"{len(self.bot.guilds)} serveurs", inline=True)
 
         if guild.owner:
             embed.add_field(
